55bea4be5b49b219395e1d492ad3664da1fd1c17
I Gusti Agung Dwigata <dwigatakusuma@gmail.com>
2025-07-31 10:09:40 +0700

    hapus file buat testing

--- app/phpinfo.php
+++
@@ -1,7 +0,0 @@
-<?php
-if (extension_loaded('ldap')) {
-    echo "LDAP extension is loaded!";
-    echo "<br>LDAP version: " . ldap_8859_to_t61("test");
-} else {
-    echo "LDAP extension is NOT loaded!";
-}
