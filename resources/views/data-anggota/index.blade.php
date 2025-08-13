24fe6b4a53d61a54a5ce570156336a861bbea456
I Gusti Agung Dwigata <dwigatakusuma@gmail.com>
2025-08-13 10:35:41 +0700

    ex-anggota super admin only

--- resources/views/data-anggota/index.blade.php
+++ resources/views/data-anggota/index.blade.php
@@ -44,10 +44,12 @@
                        class="border-b-2 py-4 px-1 text-sm font-medium {{ $activeTab === 'pengurus' ? 'border-blue-500 text-blue-600' : 'border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300' }}">
                         Data Pengurus
                     </a>
-                    <a href="{{ route('data-anggota.index', ['tab' => 'ex-anggota']) }}" 
-                       class="border-b-2 py-4 px-1 text-sm font-medium {{ $activeTab === 'ex-anggota' ? 'border-blue-500 text-blue-600' : 'border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300' }}">
-                        Ex Anggota
-                    </a>
+                    @if(Auth::user()->hasRole('ADM'))
+                     <a href="{{ route('data-anggota.index', ['tab' => 'ex-anggota']) }}" 
+                        class="border-b-2 py-4 px-1 text-sm font-medium {{ $activeTab === 'ex-anggota' ? 'border-blue-500 text-blue-600' : 'border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300' }}">
+                         Ex Anggota
+                     </a>
+                    @endif
                 </nav>
             </div>
 
