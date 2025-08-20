646c14326eb8cf4d7b5dd0fd45a6546ecb439c51
I Gusti Agung Dwigata <dwigatakusuma@gmail.com>
2025-08-20 11:06:38 +0700

    update: tampilan halaman home

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,23 +4,29 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use App\Models\SertifikatSignature; 
+use Carbon\Carbon;
 
 class HomeController extends Controller
 {
     public function index()
     {
-        // AMBIL DATA KETUA UMUM YANG AKTIF DARI DATABASE
-        // Menggabungkan tabel t_sekar_jajaran dan m_jajaran
-        $ketuaUmum = DB::table('t_sekar_jajaran')
+        $ketuaUmumInfo = DB::table('t_sekar_jajaran')
             ->join('m_jajaran', 't_sekar_jajaran.ID_JAJARAN', '=', 'm_jajaran.ID')
-            ->where('m_jajaran.NAMA_JAJARAN', 'KETUA UMUM') // Filter berdasarkan nama jabatan
-            ->where('t_sekar_jajaran.IS_AKTIF', '1') // Memastikan pengurus masih aktif
-            ->select('t_sekar_jajaran.V_NAMA_KARYAWAN as nama', 'm_jajaran.NAMA_JAJARAN as jabatan') // Pilih kolom yang diperlukan dan berikan alias
+            ->where('m_jajaran.NAMA_JAJARAN', 'KETUA UMUM')
+            ->where('t_sekar_jajaran.IS_AKTIF', '1')
+            ->select('t_sekar_jajaran.V_NAMA_KARYAWAN as nama', 'm_jajaran.NAMA_JAJARAN as jabatan')
             ->first();
+
+        $ketuaUmumSignature = SertifikatSignature::where('jabatan', 'LIKE', '%Ketua Umum%')
+                                        ->where('start_date', '<=', Carbon::now())
+                                        ->where('end_date', '>=', Carbon::now())
+                                        ->first();
 
         // Kirim data ke view
         return view('home', [
-            'ketuaUmum' => $ketuaUmum
+            'ketuaUmum' => $ketuaUmumInfo,
+            'ketuaUmumSignature' => $ketuaUmumSignature
         ]);
     }
 }
